e57a5b221b54b22a5401a8383ff8e7a36c92e71c
Joseba-Rodriguez <70901738+Joseba-Rodriguez@users.noreply.github.com>
2022-11-03 10:00:16 +0100

    Update python-nmap.py

--- python-nmap.py
+++ python-nmap.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 import nmap
-
-#Aqui le indicamos las ips(editar para hacer un rango)
+#Aqui le indica las ips(editar para hacer un rango)
 ip=input("[+] IP Objetivo ==> ")
-#Ejecutamos nmap de python y analizamos sus puertos
+#Ejecuta nmap de python y analiza sus puertos
 nm = nmap.PortScanner()
-#indicamos en una variable cual va a ser el inicio de los puertos
+#indica en una variable cual va a ser el inicio de los puertos
 puertos_abiertos="-p "
-#con nm.scan(argumentos) podremos meter todos los argunmentos necesarios que queramos que ejecute nmap, así mismo con -T1 tarda mucho, utilizamos T4 y no T5 porque da errores
+#con nm.scan(argumentos) se puede meter todos los argunmentos necesarios que queramos que ejecute nmap, así mismo con -T1 tarda mucho, utiliza T4 (mucho más rápida)y no T5 porque da errores
 results = nm.scan(hosts=ip,arguments="-sT -n -Pn -T4")
-#Inicializamos un contador
+#Inicializa un contador
 count=0
-#printeamos los resultados de IP
+#printea los resultados de IP
 print("\nHost : %s" % ip)
-#Printeamos el estado de la ip con .state de nm[ip] para saber si esta abierto o cerrado
+#Printea el estado de la ip con .state de nm[ip] para saber si esta abierto o cerrado
 print("State : %s" % nm[ip].state())
 #Por cada ip analiza y ordena todos los puertos que esten abiertos 
 for proto in nm[ip].all_protocols():
 	print("Protocol : %s" % proto)
 	print()
 	lport = nm[ip][proto].keys()
+	#ordena los puertos
 	sorted(lport)
 	for port in lport:
 		print ("port : %s\tstate : %s" % (port, nm[ip][proto][port]["state"]))
@@ -31,5 +31,7 @@ for proto in nm[ip].all_protocols():
 
 print("\nPuertos abiertos: "+ puertos_abiertos +" "+str(ip))
 
-# A partir de aquí una vez tengas la lista de todos los puertos abiertos, ya podrás ejecutar las vulnerabilidadades con:
-# nmap -sV -sC -p (pones todos los puertos de la ip)
+# A partir de aquí una vez tengas la lista de todos los puertos abiertos, ya se puede ejecutar las vulnerabilidadades con:
+# nmap -sV -sC -p (pones todos los puertos de la lista que se obtiene) Sería recomendable meter los puertos abiertos por cada ip dentro de un archivo que luego será el que se suba a la web
+#La idea es lanzar un contenedor docker para abrir una web donde se puedan obtener las ips y un apartado para añadir algunas si hicera falta 
+#Ademas habrá que lanzar un mensaje de correo electrónico donde se muestre si ha habido un cambio en la lista de ips proporcionada
