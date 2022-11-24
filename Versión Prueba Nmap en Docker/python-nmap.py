301b581d0694e0166d7d1058dd0132def7445dfb
Joseba-Rodriguez <70901738+Joseba-Rodriguez@users.noreply.github.com>
2022-11-24 09:43:11 +0100

    Update python-nmap.py

--- Versión Prueba Nmap en Docker/python-nmap.py
+++ Versión Prueba Nmap en Docker/python-nmap.py
@@ -1,31 +1,36 @@
 import nmap
 #Aqui le indica las ips(editar para hacer un rango)
-ip="169.254.169.254"
+with open ("ips.txt","r") as file:
+        filecontents = file.read()
+        lists = []
+        for line in open('ips.txt'):
+                lists.append(line.strip())
+        for ip in lists:
 #Ejecuta nmap de python y analiza sus puertos
-nm = nmap.PortScanner()
+                nm = nmap.PortScanner()
 #indica en una variable cual va a ser el inicio de los puertos
-puertos_abiertos="-p "
-#con nm.scan(argumentos) se puede meter todos los argunmentos necesarios que queramos que ejecute nmap, así mismo con -T1 tarda mucho, utiliza T4 (mucho más rápida)y no T5 porque da errores
-results = nm.scan(hosts=ip,arguments="-sT -n -Pn -T4")
+                puertos_abiertos="-p "
+#con nm.scan(argumentos) se puede meter todos los argunmentos necesarios que queramos que ejecute nmap, así mismo con>
+                results = nm.scan(hosts=ip,arguments="-sT -n -Pn -T4")
 #Inicializa un contador
-count=0
+                count=0
 #printea los resultados de IP
-print("\nHost : %s" % ip)
+                print("\nHost : %s" % ip)
 #Printea el estado de la ip con .state de nm[ip] para saber si esta abierto o cerrado
-print("State : %s" % nm[ip].state())
+                print("State : %s" % nm[ip].state())
 #Por cada ip analiza y ordena todos los puertos que esten abiertos 
-for proto in nm[ip].all_protocols():
-	print("Protocol : %s" % proto)
-	print()
-	lport = nm[ip][proto].keys()
-	#ordena los puertos
-	sorted(lport)
-	for port in lport:
-		print ("port : %s\tstate : %s" % (port, nm[ip][proto][port]["state"]))
-		if count==0:
-			puertos_abiertos=puertos_abiertos+str(port)
-			count=1
-		else:
-			puertos_abiertos=puertos_abiertos+","+str(port)
+                for proto in nm[ip].all_protocols():
+                        print("Protocol : %s" % proto)
+                        print()
+                        lport = nm[ip][proto].keys()
+        #ordena los puertos
+                        sorted(lport)
+                        for port in lport:
+                                print ("port : %s\tstate : %s" % (port, nm[ip][proto][port]["state"]))
+                                if count==0:
+                                        puertos_abiertos=puertos_abiertos+str(port)
+                                        count=1
+                                else:
+                                        puertos_abiertos=puertos_abiertos+","+str(port)
 
-print("\nPuertos abiertos: "+ puertos_abiertos +" "+str(ip))
+                print("\nPuertos abiertos: "+ puertos_abiertos +" "+str(ip))
