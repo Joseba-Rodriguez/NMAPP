3855a2167f4fb4f8195d28940aa73208202bc9d3
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2022-12-14 09:54:08 +0100

    Update index.php

--- Versión Prueba Nmap en Docker/web/html/index.php
+++ Versión Prueba Nmap en Docker/web/html/index.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>PostgreSQL</title>
-	
-	<style>
-		table, th, td{
-			border: 1px solid black;
-		}
-	</style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>NMAP-ITP</title>
+
+        <style>
+                table, th, td{
+                        border: 1px solid black;
+                }
+        </style>
 </head>
 <body>
-	<form action="conexion.php" method="POST">
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <form action="conexion.php" method="POST">
 
 <?php 
 
 $file = file('datos.xml');
-$conexion = pg_connect("host=db port=5432 dbname=nmap user=root password=root");
+#$conexion = pg_connect("host=db port=5432 dbname=nmap user=root password=root");
 
 $ip;
 $mac;
 $vendor;
+$cve;
 $hostname;
 $port;
 $portArray = array();
@@ -30,6 +34,7 @@ $timestamp;
 
 
 foreach ($file as $line){
+    //ip
     if (strpos($line, 'addrtype="ipv4"') == TRUE) {
         preg_match('/addr=".* addrtype/',$line,$results);
         $ip = implode (" ", $results);
@@ -38,7 +43,7 @@ foreach ($file as $line){
         print "<br><strong><u>Device</u></strong></br>";
         print "IP Address: $ip<br>";
     }
-    
+    //vendor
     if (strpos($line, 'addrtype="mac"') == TRUE) {
         preg_match('/vendor=".*/',$line,$results);
         $vendor = implode (" ", $results);
@@ -46,7 +51,7 @@ foreach ($file as $line){
         $vendor = rtrim($vendor, '"');
         print "Vendor: $vendor<br>";
     }
-
+    //mac
     if (strpos($line, 'addrtype="mac"') == TRUE) {
         preg_match('/addr=".* addrtype/',$line,$results);
         $mac = implode (" ", $results);
@@ -54,7 +59,7 @@ foreach ($file as $line){
         $mac = rtrim($mac, '" addrtype');
         print "MAC Address: $mac<br>";
     }
-
+    //hostname
     if (strpos($line, 'type="PTR"') == TRUE) {
         preg_match('/name=".*" type/',$line,$results);
         $hostname = implode (" ", $results);
@@ -63,7 +68,7 @@ foreach ($file as $line){
         $hostname = rtrim($hostname, '"');
         print "Hostname: $hostname<br>";
     }
-    
+    //puertos
     if (strpos($line, 'portid="') == TRUE) {
         preg_match('/portid=".*><state/',$line,$results);
         $port = implode(" ", $results);
@@ -71,6 +76,15 @@ foreach ($file as $line){
         $port = rtrim($port, '"><state');
         print "Port: $port<br>";
         array_push($portArray, $port);
+    }
+    
+    //cve
+    if (strpos($line, 'key="id"') == TRUE) {
+        preg_match('/key="id">.*</',$line,$results);
+        $cve = implode(" ", $results);
+        $cve = ltrim($cve, 'key="id">');
+        $cve = rtrim($cve, '"</elem');
+        print "CVE: $cve https://vulners.com/cve/$cve<br>";
     }
     
     if (strpos($line, '/host>') == TRUE) {
@@ -88,6 +102,6 @@ foreach ($file as $line){
     }
 }
 ?>
-	</form>
+        </form>
 </body>
 </html>
