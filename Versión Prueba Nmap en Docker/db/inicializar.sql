0e27b5cc0437c256fd87c99c278cd92c743d6312
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2022-12-15 11:28:06 +0100

    Update inicializar.sql

--- Versión Prueba Nmap en Docker/db/inicializar.sql
+++ Versión Prueba Nmap en Docker/db/inicializar.sql
@@ -1,4 +1,9 @@
-CREATE TABLE entradas (id int,
-	fecha TIMESTAMP PRIMARY KEY,
-	nombre varchar(10)
+create table nmapScan (
+    ip varchar(32) not null check (ip > 0),
+    mac varchar(32) not null,
+    vendor varchar(50) not null,
+    hostname varchar(100)not null,
+    ports integer not null check (ports > 0),
+    timestamp time not null,
+    primary key (ip)
 );
