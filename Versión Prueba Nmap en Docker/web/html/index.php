311f8c386d38c809144458ff968ab679f3407eba
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2022-12-15 11:59:09 +0100

    Conexion bbdd

--- Versión Prueba Nmap en Docker/web/html/index.php
+++ Versión Prueba Nmap en Docker/web/html/index.php
@@ -17,10 +17,26 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <form action="conexion.php" method="POST">
 
-<?php 
+<?php
+
+
+$usuario="root";
+$pass="root";
+$host"db";
+$db="nmap";
 
 $file = file('datos.xml');
-#$conexion = pg_connect("host=db port=5432 dbname=nmap user=root password=root");
+
+function conectar_PostgreSQL( $usuario, $pass, $host, $bd )
+    {
+         $conexion = pg_connect( "user=".$usuario." ".
+                                "password=".$pass." ".
+                                "host=".$host." ".
+                                "dbname=".$bd
+                               ) or die( "Error al conectar: ".pg_last_error() );
+        return $conexion;
+    }
+
 
 $ip;
 $mac;
@@ -77,7 +93,7 @@ foreach ($file as $line){
         print "Port: $port<br>";
         array_push($portArray, $port);
     }
-    
+
     //cve
     if (strpos($line, 'key="id"') == TRUE) {
         preg_match('/key="id">.*</',$line,$results);
@@ -86,11 +102,13 @@ foreach ($file as $line){
         $cve = rtrim($cve, '"</elem');
         print "CVE: $cve https://vulners.com/cve/$cve<br>";
     }
-    
+
     if (strpos($line, '/host>') == TRUE) {
         $timestamp = time();
         $portList = implode(", ", $portArray);
-        $sql = "insert into nmap(ip,mac,vendor,hostname,ports,timestamp) values ('$ip','$mac','$vendor','$hostname','$portList','$timestamp')";
+        $sql = "insert into nmapScan(ip,mac,vendor,hostname,ports,timestamp) values ('$ip','$mac','$vendor','$hostname','$portList','$timestamp')";
+
+    pg_query($conexion,$sql);
 
     $ip = " ";
     $mac = " ";
