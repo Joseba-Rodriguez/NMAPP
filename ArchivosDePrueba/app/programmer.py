5bde398b4c3a224b00da2dd62d5e2b1d3d8604da
joseba-rodriguez <joseba.rodriguez01@gmail.com>
2023-01-12 22:12:28 +0100

    Programmer added

---
+++ ArchivosDePrueba/app/programmer.py
@@ -0,0 +1,26 @@
+import sys
+import schedule
+import time 
+import os
+
+def DBVulners():
+     os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git ; cd ./app/vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles")
+
+def Analyzer():
+    os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git ")
+
+    os.system("chmod 777 ./app/ips.txt ; chmod 777 ./app/datos.xml ")
+
+    os.system("nmap -sV --script nmap-vulners --script-args vulscandb=scipvuldb.csv -sV -stats-every 2s -iL ./app/ips.txt -oX ./app/datos.xml")
+
+    os.system(" python ./app/Storer.py ./app/datos.xml ")
+
+
+schedule.every(10).minutes.do(DBVulners)
+schedule.every(1).minutes.do(Analyzer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+    
