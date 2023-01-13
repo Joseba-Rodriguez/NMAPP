1d89037ff05518e8c96b54424ed0f16dd5365456
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-01-13 10:39:22 +0100

    Update programmer.py

--- ArchivosDePrueba/app/programmer.py
+++ ArchivosDePrueba/app/programmer.py
@@ -12,7 +12,7 @@ def DBVulners():
      os.system("cd /usr/share/nmap/scripts/vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles.sh")
 
 def Analyzer():
-    os.system("nmap -p- -sV --script nmap-vulners --script-args vulscandb=scipvuldb.csv -sV -stats-every 2s -iL ./ap>
+    os.system("nmap -sV --script nmap-vulners --script-args vulscandb=scipvuldb.csv -sV -stats-every 2s -iL ./ips.txt -oX ./web/datos.xml")
 
     os.system(" python ./app/Storer.py ./app/datos.xml ")
 
