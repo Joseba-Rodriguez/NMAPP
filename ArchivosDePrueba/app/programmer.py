ce56660da89d6fd9fb7eec9e51e2b3b1983cfa74
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-01-13 10:30:12 +0100

    Update programmer.py

--- ArchivosDePrueba/app/programmer.py
+++ ArchivosDePrueba/app/programmer.py
@@ -1,26 +1,29 @@
+
 import sys
 import schedule
-import time 
+import time
 import os
 
+
+os.system("cd /usr/share/nmap/scripts/ ;git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https:/>
+os.system("chmod 777 ./app/ips.txt ; chmod 777 ./app/datos.xml ")
+
 def DBVulners():
-     os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git ; cd ./app/vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles")
+     os.system("cd /usr/share/nmap/scripts/vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles.sh")
 
 def Analyzer():
-    os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git ")
-
-    os.system("chmod 777 ./app/ips.txt ; chmod 777 ./app/datos.xml ")
-
-    os.system("nmap -sV --script nmap-vulners --script-args vulscandb=scipvuldb.csv -sV -stats-every 2s -iL ./app/ips.txt -oX ./app/datos.xml")
+    os.system("nmap -p- -sV --script nmap-vulners --script-args vulscandb=scipvuldb.csv -sV -stats-every 2s -iL ./ap>
 
     os.system(" python ./app/Storer.py ./app/datos.xml ")
 
 
-schedule.every(10).minutes.do(DBVulners)
+schedule.every(5).minutes.do(DBVulners)
 schedule.every(1).minutes.do(Analyzer)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+
 
     
