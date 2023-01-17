67eecfdd8615ab232b87591740146cf39f218b87
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-01-17 11:14:49 +0100

    Add files via upload

---
+++ ArchivosDePrueba/app/Discovery.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Product Admin - Dashboard HTML Template</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+    <!-- https://fonts.google.com/specimen/Roboto -->
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <!-- https://fontawesome.com/ -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- https://getbootstrap.com/ -->
+    <link rel="stylesheet" href="css/templatemo-style.css">
+    <!--
+	Product Admin CSS Template
+	https://templatemo.com/tm-524-product-admin
+	-->
+</head>
+
+<body id="reportsPage">
+    <div class="" id="home">
+        <nav class="navbar navbar-expand-xl">
+            <div class="container h-100">
+                <a class="navbar-brand" href="index.php">
+                    <h1 class="tm-site-title mb-0"> ITP Aero - TFG </h1>
+                </a>
+                <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars tm-nav-icon"></i>
+                </button>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link d-block" href="login.html">
+                                Admin, <b>Logout</b>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <p class="text-white mt-5 mb-5">Welcome back, <b>Admin</b></p>
+                </div>
+            </div>
+            <!-- row -->
+            <div class="row tm-content-row">
+               
+                        <?php
+                        include('Connection.php');
+                        #Appear: data that is in nmapScan but not in lastAnalyze 
+                        $query = "SELECT * FROM nmapScan as n WHERE NOT EXISTS(SELECT * FROM lastAnalyze AS L WHERE n.ip = L.ip)";
+                        $result = pg_query($conexion, $query);
+                        $arr = pg_fetch_all($result);
+                        echo'
+                        <div class="col-12 tm-block-col">
+                        <div class="tm-bg-primary-dark tm-block tm-block-taller tm-block-scroll">
+                        <h2 class="tm-block-title">Appear</h2>
+                            <table class="table">
+                                <tr>
+                                 <th>ip</th>
+                                 <th>hostname</th>
+                                 <th>port</th>
+                                 <th>protocol</th>
+                                 <th>service</th>
+                                 <th>version</th>
+                                </tr>';
+                        foreach($arr as $array)
+                            {
+                                echo'<tr>
+                                    <td>'. $array['ip'].'</td>
+                                    <td>'. $array['hostname'].'</td>
+                                    <td>'. $array['port'].'</td>
+                                    <td>'. $array['protocol'].'</td>
+                                    <td>'. $array['service'].'</td>
+                                    <td>'. $array['version'].'</td>
+                                    </tr>';
+                            }
+                            echo'</table>';
+                            ?>
+                    </div>
+                </div>
+            </div>
+                <div class="row tm-content-row">
+                            <?php
+                            #Lost :data that is in LastAnalyze but not in nmapScan 
+                            $query = "SELECT * FROM lastanalyze as n WHERE NOT EXISTS(SELECT * FROM nmapscan AS L WHERE n.ip = L.ip)";
+                            $result = pg_query($conexion, $query);
+                            $arr = pg_fetch_all($result);
+                            echo'
+                            <div class="col-12 tm-block-col">
+                        <div class="tm-bg-primary-dark tm-block tm-block-taller tm-block-scroll">
+                        <h2 class="tm-block-title">Lost</h2>
+                                <table class="table">
+                                    <tr>
+                                    <th>ip</th>
+                                    <th>hostname</th>
+                                    <th>port</th>
+                                    <th>protocol</th>
+                                    <th>service</th>
+                                    <th>version</th>
+                                    </tr>';
+                            foreach($arr as $array)
+                                {
+                                    echo'<tr>
+                                        <td>'. $array['ip'].'</td>
+                                        <td>'. $array['hostname'].'</td>
+                                        <td>'. $array['port'].'</td>
+                                        <td>'. $array['protocol'].'</td>
+                                        <td>'. $array['service'].'</td>
+                                        <td>'. $array['version'].'</td>
+                                        </tr>';
+                                }
+                                echo'</table>';
+                                ?>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="row tm-content-row">
+                                <?php
+                                #Stay:
+                                $query = "SELECT * FROM nmapScan INTERSECT  SELECT * from lastanalyze";
+                                $result = pg_query($conexion, $query);
+                                $arr = pg_fetch_all($result);
+                                echo'<div class="col-12 tm-block-col">
+                                <div class="tm-bg-primary-dark tm-block tm-block-taller tm-block-scroll">
+                                <h2 class="tm-block-title">Stay</h2>
+                                    <table class="table">
+                                        <tr>
+                                        <th>ip</th>
+                                        <th>hostname</th>
+                                        <th>port</th>
+                                        <th>protocol</th>
+                                        <th>service</th>
+                                        <th>version</th>
+                                        </tr>';
+                                foreach($arr as $array)
+                                    {
+                                        echo'<tr>
+                                            <td>'. $array['ip'].'</td>
+                                            <td>'. $array['hostname'].'</td>
+                                            <td>'. $array['port'].'</td>
+                                            <td>'. $array['protocol'].'</td>
+                                            <td>'. $array['service'].'</td>
+                                            <td>'. $array['version'].'</td>
+                                            </tr>';
+                                    }
+                                    echo'</table>';
+                            ?>     
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="tm-footer row tm-mt-small">
+            <div class="col-12 font-weight-light">
+                <p class="text-center text-white mb-0 px-4 small">
+                    Copyright &copy; <b>2023</b> All rights reserved. 
+                    
+                    Design: <a rel="nofollow noopener" href="https://templatemo.com" class="tm-footer-link">Template Mo</a>
+                </p>
+            </div>
+        </footer>
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!-- https://jquery.com/download/ -->
+    <script src="js/moment.min.js"></script>
+    <!-- https://momentjs.com/ -->
+    <script src="js/Chart.min.js"></script>
+    <!-- http://www.chartjs.org/docs/latest/ -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- https://getbootstrap.com/ -->
+    <script src="js/tooplate-scripts.js"></script>
+    <script>
+        Chart.defaults.global.defaultFontColor = 'white';
+        let ctxLine,
+            ctxBar,
+            ctxPie,
+            optionsLine,
+            optionsBar,
+            optionsPie,
+            configLine,
+            configBar,
+            configPie,
+            lineChart;
+        barChart, pieChart;
+        // DOM is ready
+        $(function () {
+            drawLineChart(); // Line Chart
+            drawBarChart(); // Bar Chart
+            drawPieChart(); // Pie Chart
+
+            $(window).resize(function () {
+                updateLineChart();
+                updateBarChart();                
+            });
+        })
+    </script>
+</body>
+
+</html>
---
+++ ArchivosDePrueba/app/programmer.py
@@ -0,0 +1,29 @@
+
+import sys
+import schedule
+import time
+import os
+
+
+def DBVulners():
+     os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git ; cd ./app/vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles.sh")
+
+def Analyzer():
+    os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git ; cd vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles")
+
+
+    os.system("nmap -sV --script nmap-vulners --script-args vulscandb=scipvuldb.csv -sV -stats-every 2s -iL ./app/ips.txt -oX ./app/datos.xml")
+
+    os.system(" python ./app/Storer.py ./app/datos.xml ")
+
+
+schedule.every(5).minutes.do(DBVulners)
+schedule.every(1).minutes.do(Analyzer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+    
