009a91ef7a4f63658cc5d12db15f31ec0ea32014
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-01-17 11:12:37 +0100

    Delete ArchivosDePrueba directory

--- ArchivosDePrueba/app/python-nmap.py
+++
@@ -1,6 +0,0 @@
-import os
-#Descargas de las bases de datos
-
-os.system("nmap -sV --script nmap-vulners --script-args vulscandb=scipvuldb.csv -sV -stats-every 2s -iL ./app/ips.txt -oX ./app/datos.xml")
-
-os.system(" python3 ./app/Storer.py ./app/datos.xml ")
