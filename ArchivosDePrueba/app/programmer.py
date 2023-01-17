ff2cd13e093286c00ce4edc4a2e0d4cc5946e028
joseba-rodriguez <joseba.rodriguez01@gmail.com>
2023-01-17 10:38:07 +0100

    VueltaALasDescargas

--- ArchivosDePrueba/app/programmer.py
+++ ArchivosDePrueba/app/programmer.py
@@ -5,7 +5,7 @@ import time
 import os
 
 
-
+os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git")
 def DBVulners():
      os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/vulnersCom/nmap-vulners.git ; git clone https://github.com/scipag/vulscan.git ; cd ./app/vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles.sh")
 
