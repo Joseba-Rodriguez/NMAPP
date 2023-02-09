993143e08371d9300c5f0ab96d5700ae4b0b0cbd
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-02-09 11:11:14 +0100

    Update pdfReport.py

--- ArchivosDePrueba/app/pdfReport.py
+++ ArchivosDePrueba/app/pdfReport.py
@@ -3,7 +3,7 @@ from fpdf import FPDF
 import psycopg2
 import smtplib
 
-# Establecer conexión a la base de datos
+# Connect to the database
 conn = psycopg2.connect(
      host="db",
      database="nmap",
@@ -11,30 +11,30 @@ conn = psycopg2.connect(
      password="root"
      )
 
-# Crear un cursor
+# Create a cursor
 cursor = conn.cursor()
 
-# Ejecutar una consulta SQL
+# Execute a SQL query
 cursor.execute("SELECT * FROM nmapIndividual as n WHERE NOT EXISTS(SELECT * FROM lastAnalyze AS L WHERE n.ip = L.ip)")
 
-# Recoger los resultados de la consulta
+# Collect the results of the consultation
 results = cursor.fetchall()
 
-# Convertir los resultados en un DataFrame de Pandas
+# Convert the results to a Pandas DataFrame
 df = pd.DataFrame(results, columns=["ip", "hostname", "port", "protocol", "service", "version", "vuln"])
 
-# Crear una instancia de la clase FPDF
+# Create an instance of the FPDF class
 pdf = FPDF()
 
-# Añadir una página
+# Add a page
 pdf.add_page()
 
-# Establecer el tipo de letra y tamaño
+# Set font and size
 pdf.set_font("Arial", size=12)
 
-# Iterar sobre las filas del DataFrame
+# Iterate over DataFrame rows
 for i, row in df.iterrows():
-    # Añadir el contenido de cada columna a la página PDF
+    #  Add the contents of each column to the PDF page
     pdf.cell(200, 10, txt=row["ip"], ln=i, align="C")
     pdf.cell(200, 10, txt=row["hostname"], ln=i, align="C")
     pdf.cell(200, 10, txt=row["port"], ln=i, align="C")
@@ -43,38 +43,38 @@ for i, row in df.iterrows():
     pdf.cell(200, 10, txt=row["version"], ln=i, align="C")
     pdf.cell(200, 10, txt=row["vuln"], ln=i, align="C")
 
-# Guardar el archivo PDF
+# Save the PDF file
 pdf.output("./app/resources/data.pdf")
 
-# Cerrar la conexión a la base de datos
+# Close the connection to the database
 conn.close()
 
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# Crear el mensaje de correo electrónico
+# Create the e-mail message
 msg = MIMEMultipart()
 msg['From'] = "nmappreporte@gmail.com"
 msg['To'] = "joseba.rodriguez01@gmail.com"
 msg['Subject'] = "NMAPP - Reporte de nuevos puertos encontrados"
 
-# Agregar el cuerpo del mensaje de correo electrónico
+# Add the body of the e-mail message
 msg.attach(MIMEText("Se adjunta pdf con los puertos encontrados por la aplicación NMAPP - Joseba Rodríguez"))
 
-# Abrir el archivo PDF y agregarlo como un adjunto al mensaje de correo electrónico
+# Open the PDF file and add it as an attachment to the email message
 with open("./app/resources/data.pdf", "rb") as f:
     pdf = MIMEApplication(f.read(), _subtype="pdf")
     pdf.add_header('content-disposition', 'attachment', filename=".NMAPP.pdf")
     msg.attach(pdf)
 
-# Autenticarse en el servidor de correo de Gmail
+# Authenticate to the Gmail mail server
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("nmappreporte@gmail.com", "nynyncvglafjihct")
 
-# Enviar el correo electrónico
+# Send the email 
 server.sendmail(msg['From'], msg['To'], msg.as_string())
 
-# Cerrar la conexión con el servidor de correo
+# Close the connection with the server
 server.quit()
