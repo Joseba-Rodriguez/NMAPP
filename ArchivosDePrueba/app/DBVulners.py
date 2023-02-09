ba717808c79d6dac5ad6734ae5f49f68f40e5000
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-02-09 10:35:20 +0100

    Delete DBVulners.py

--- ArchivosDePrueba/app/DBVulners.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system("cd /usr/share/nmap/scripts/ ; git clone https://github.com/scipag/vulscan.git ; cd ./app/vulscan/utilities/updater/ ; chmod +x updateFiles.sh ; ./updateFiles.sh")
