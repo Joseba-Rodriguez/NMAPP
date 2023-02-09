728189611a6b79b292215e0cac6b15ce28b134e4
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-02-09 10:35:03 +0100

    Update Connection.php

--- ArchivosDePrueba/app/Connection.php
+++ ArchivosDePrueba/app/Connection.php
@@ -1,8 +1,10 @@
 <?php
 
+//This is the main connection to the database executed in the container db
+
 $usuario="root";
 $pass="root";
-$host="db";
+$host="db"; //We use de "db" host because is the name of the container that is launched
 $db="nmap";
 $port=5432;
 
