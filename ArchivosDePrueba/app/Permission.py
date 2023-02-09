3384dde71448958d6ed796526c3e9366f6873c1e
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-02-09 10:46:00 +0100

    Update Permission.py

--- ArchivosDePrueba/app/Permission.py
+++ ArchivosDePrueba/app/Permission.py
@@ -1,9 +1,9 @@
 import time
 import os ,sys, stat
 
-os.chmod("./ips.txt", stat.S_IRWXO) #establecer permisos para ips.txt
-os.chmod("./datos.xml", stat.S_IRWXO) #establecer permisos para datos.txt
-os.chmod("./Storer.py", stat.S_IRWXO) #establecer permisos para Storer.py
-os.chmod("./programmer.py", stat.S_IRWXO) #establecer permisos para programmer.py
-os.chmod("./DBVulners.py", stat.S_IRWXO) #establecer permisos para programmer.py
-os.chmod("./Analyzer.py", stat.S_IRWXO) #establecer permisos para programmer.py
+os.chmod("./ips.txt", stat.S_IRWXO) #permissios for ips.txt
+os.chmod("./datos.xml", stat.S_IRWXO) #permissios for datos.txt
+os.chmod("./Storer.py", stat.S_IRWXO) #permissios for Storer.py
+os.chmod("./programmer.py", stat.S_IRWXO) #permissios for programmer.py
+os.chmod("./DBVulners.py", stat.S_IRWXO) #permissios for programmer.py
+os.chmod("./Analyzer.py", stat.S_IRWXO) #permissios for programmer.py
