5d8aa784384c7baa19e88b45acdd79999883916d
joseba-rodriguez <joseba.rodriguez01@gmail.com>
2023-02-15 20:31:22 +0100

    funcionaCSV

--- ArchivosDePrueba/app/csv.php
+++ ArchivosDePrueba/app/csv.php
@@ -1,40 +1,25 @@
 <?php
 if (isset($_POST['download_data'])) {
-    $table = $_POST['download_data'];
+  $table = $_POST['download_data'];
 
-    // Connect to database
-    require "Connection.php";
-    
-    // Escape the table name to prevent SQL injection
-    $table = pg_escape_identifier($table);
+  // Connect to database
+  require "Connection.php";
+  // Get data from table
+  $result = pg_query($conexion, "SELECT * FROM $table");
+  $data = pg_fetch_all($result);
 
-    // Get data from table
-    $result = pg_query($conexion, "SELECT * FROM $table");
-    if (!$result) {
-        // Query failed
-        echo "Query failed: " . pg_last_error($conexion);
-        exit;
-    }
-    $data = pg_fetch_all($result);
-    if (!$data) {
-        // No data found
-        echo "No data found in table '$table'";
-        exit;
-    }
+  // Set header for downloading file
+  header("Content-type: text/csv");
+  header("Content-Disposition: attachment; filename=$table Scan.csv");
+  header("Pragma: no-cache");
+  header("Expires: 0");
 
-    // Set header for downloading file
-    header("Content-type: text/csv");
-    header("Content-Disposition: attachment; filename=${table}_Scan.csv");
-    header("Pragma: no-cache");
-    header("Expires: 0");
-
-    // Output data in CSV format
-    $output = fopen("php://output", "w");
-    fputcsv($output, array_keys($data[0]));
-    foreach ($data as $row) {
-        fputcsv($output, $row);
-    }
-    fclose($output);
-    exit;
+  // Output data in CSV format
+  $output = fopen("php://output", "w");
+  fputcsv($output, array_keys($data[0]));
+  foreach ($data as $row) {
+    fputcsv($output, $row);
+  }
+  fclose($output);
 }
 ?>
