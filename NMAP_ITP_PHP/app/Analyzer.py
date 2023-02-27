fe578cf8969045863399a6f0151b13235ec55ef9
Josebarv <70901738+Joseba-Rodriguez@users.noreply.github.com>
2023-02-27 12:07:54 +0100

    Delete NMAP_ITP_PHP directory

--- NMAP_ITP_PHP/app/Analyzer.py
+++
@@ -1,7 +0,0 @@
-import os, stat
-
-#This is the main command of nmap
-os.system("nmap -sV --script vulners --script-args mincvss=5.0 -sV -stats-every 2s -iL ./app/ipsReporte.txt -oX ./app/datos.xml")
-
-#With the previous export, we call a new python to parse the data
-os.system(" python3 ./app/storer.py 2")
